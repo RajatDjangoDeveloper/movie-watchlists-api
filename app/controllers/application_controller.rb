a344605d59300c8e3a35baef0054b61ab701ca3d
rajat j <cseengin@gamil.com>
2023-09-25 21:52:52 +0530

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
   end
+
+rescue_from CanCan::AccessDenied do |exception|
+  exception.default_message = "You are not authorized to perform this task"
+  respond_to do |format|
+    format.json { head :forbidden }
+    format.html { redirect_to root_path, alert: exception.message }
+  end
+end
+
 end
